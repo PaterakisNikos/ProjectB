e543aa4f352e970e6ef85fd0b22bc57996333526
VMormoris <vmormoris2014@gmail.com>
2021-01-05 12:27:09 +0200

    Fixing filepath to resources

--- src/main/java/com/example/projectb/staticcontent/StaticContentService.java
+++ src/main/java/com/example/projectb/staticcontent/StaticContentService.java
@@ -10,26 +10,26 @@ public class StaticContentService {
 	
 
 	//----HTMLS
-	private String loghtml = new String(readAllBytesJava7("log.html"));
-	private String movieshtml = new String(readAllBytesJava7("movies.html"));
-	private String bookmarkshtml = new String(readAllBytesJava7("bookmarks.html"));
+	private String loghtml = new String(readAllBytesJava7("resources/pages/log.html"));
+	private String movieshtml = new String(readAllBytesJava7("resources/pages/movies.html"));
+	private String bookmarkshtml = new String(readAllBytesJava7("resources/pages/bookmarks.html"));
 
 	//CSS
-	private String logcss = new String(readAllBytesJava7("log-style.css"));
-	private String moviescss = new String(readAllBytesJava7("movies-style.css"));
-	private String bookmarkscss = new String(readAllBytesJava7("bookmarks-style.css"));
+	private String logcss = new String(readAllBytesJava7("resources/styles/log-style.css"));
+	private String moviescss = new String(readAllBytesJava7("resources/styles/movies-style.css"));
+	private String bookmarkscss = new String(readAllBytesJava7("resources/styles/bookmarks-style.css"));
 
 	//JS
-	private String generaljs = new String(readAllBytesJava7("general-logic.js"));
-	private String logjs = new String(readAllBytesJava7("log-logic.js"));
-	private String moviejs = new String(readAllBytesJava7("movies-logic.js"));
-	private String bookmarksjs = new String(readAllBytesJava7("bookmarks-logic.js"));
+	private String generaljs = new String(readAllBytesJava7("resources/logic/general-logic.js"));
+	private String logjs = new String(readAllBytesJava7("resources/logic/log-logic.js"));
+	private String moviejs = new String(readAllBytesJava7("resources/logic/movies-logic.js"));
+	private String bookmarksjs = new String(readAllBytesJava7("resources/logic/bookmarks-logic.js"));
 	
 	//Images
-	private byte [] logoImage = readAllBytesJava7("Logo.png");
-	private byte [] userImage = readAllBytesJava7("user.png");
-	private byte [] notBookmarked = readAllBytesJava7("emptyStar.png");
-	private byte [] bookmarked = readAllBytesJava7("fillStar.png");
+	private byte [] logoImage = readAllBytesJava7("resources/images/Logo.png");
+	private byte [] userImage = readAllBytesJava7("resources/images/user.png");
+	private byte [] notBookmarked = readAllBytesJava7("resources/images/emptyStar.png");
+	private byte [] bookmarked = readAllBytesJava7("resources/images/fillStar.png");
 	
 	public String getBookmarksPage() { return bookmarkshtml; }
 	public String getBookmarksStyle() { return bookmarkscss; }
